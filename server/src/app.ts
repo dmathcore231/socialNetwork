4edd9cb82688e4b43f566068fa7255b37606b09b
@dmathcore231 <dmathcore231@gmail.com>
2024-03-20 14:09:27 +0300

    init(server): init base template

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express'
+import mongoose, { Mongoose, mongo } from 'mongoose'
+
+
+const app = express()
+
+app.use((_: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next()
+})
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/socialNetwork')
+    app.listen(3000, () => {
+      console.log('Server started on http://localhost:3000')
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
+
+process.on('SIGINT', async () => {
+  await mongoose.disconnect()
+  console.log('Server closed')
+  process.exit()
+})
